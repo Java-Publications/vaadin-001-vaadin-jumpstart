baf7be535aec7f4916f2a2b174e54f05c1a6f68f
Sven Ruppert <sven.ruppert@gmail.com>
2017-04-10 10:07:13 +0200

    added first JDBC DAO code, including HsqlDB Inmemory jUNit tests

---
+++ modules/backend/storage/plainjdbc/src/main/java/org/rapidpm/vaadin/jumpstart/backend/storage/plainjdbc/NestedBuilder.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.rapidpm.vaadin.jumpstart.backend.storage.plainjdbc;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public abstract class NestedBuilder<T, V> {
+  protected T parent;
+
+  /**
+   * To get the parent builder
+   *
+   * @return T the instance of the parent builder
+   */
+  public T done() {
+    Class<?> parentClass = parent.getClass();
+    try {
+      V build = this.build();
+      String methodname = "with" + build.getClass().getSimpleName();
+      Method method = parentClass.getDeclaredMethod(methodname, build.getClass());
+      final boolean accessible = method.isAccessible();
+      method.setAccessible(true);
+      method.invoke(parent, build);
+      method.setAccessible(accessible);
+    } catch (NoSuchMethodException
+        | IllegalAccessException
+        | InvocationTargetException e) {
+      e.printStackTrace();
+    }
+    return parent;
+  }
+
+  public abstract V build();
+
+  /**
+   * @param parent
+   *
+   * @return
+   */
+  public <P extends NestedBuilder<T, V>> P withParentBuilder(T parent) {
+    this.parent = parent;
+    return (P) this;
+  }
+}
---
+++ modules/backend/storage/plainjdbc/src/test/java/junit/org/rapidpm/vaadin/jumpstart/backend/storage/plainjdbc/dao/v001/UserDAOBaseTest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package junit.org.rapidpm.vaadin.jumpstart.backend.storage.plainjdbc.dao.v001;
+
+
+import junit.org.rapidpm.vaadin.jumpstart.backend.storage.plainjdbc.HsqlBaseTest;
+import junit.org.rapidpm.vaadin.jumpstart.backend.storage.plainjdbc.InMemoryHsqldbBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.rapidpm.vaadin.jumpstart.backend.storage.plainjdbc.JDBCConnectionPools;
+
+
+public class UserDAOBaseTest extends HsqlBaseTest {
+
+  final JDBCConnectionPools pools = new JDBCConnectionPools();
+
+  @Override
+  public JDBCConnectionPools pools() {
+    return pools;
+  }
+
+  @Override
+  public String[] createSQLInitScriptArray() {
+    return new String[]{
+        "CLEAR_SCHEMA.sql", "CREATE_TABLE_EXAMPLE.sql"
+    };
+
+  }
+
+  private InMemoryHsqldbBuilder.ServerResult serverResult;
+
+  @Before
+  public void setUp() throws Exception {
+    System.out.println("poolname = " + poolname());
+
+    serverResult = InMemoryHsqldbBuilder.newBuilder()
+        .withDbName("testDB")
+        .withRandomPort()
+        .build();
+
+
+
+    startPoolsAndConnect(poolname(), serverResult.getUrl());
+    initSchema(poolname());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    pools().shutdownPool(poolname());
+
+  }
+
+  @Override
+  public Class baseTestClass() {
+    return UserDAOBaseTest.class;
+  }
+
+}
